502cacf4686c7d495e0a75dd6db6c14c55a1ee95
sxf796 <sxf796@fa6e4dec-783f-4bee-b038-fdf0b38339eb>
2014-03-09 16:21:09 +0000

    git-svn-id: https://codex.cs.bham.ac.uk/svn/msc-workshop/Osaka@10 fa6e4dec-783f-4bee-b038-fdf0b38339eb

---
+++ QuizProgram_src/quizObject/QuizRequest.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class QuizRequest implements Serializable {
+
+		public static final long serialVersionUID = 42L;
+
+		private static long quizID;
+
+		public QuizRequest(long quizID){
+
+			this.quizID = quizID;
+
+		}//end of constructor
+
+		public long getQuizID(){
+ 			return quizID;
+		}
+
+}//end of class
