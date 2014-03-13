e0eb6f97b41db9e90a9ee9798043a47a1e4d75d0
bxc077 <bxc077@fa6e4dec-783f-4bee-b038-fdf0b38339eb>
2014-03-13 16:55:21 +0000

    Update Score object - added username
    git-svn-id: https://codex.cs.bham.ac.uk/svn/msc-workshop/Osaka@46 fa6e4dec-783f-4bee-b038-fdf0b38339eb

--- QuizProgram_src/quizObject/Score.java
+++ QuizProgram_src/quizObject/Score.java
@@ -10,13 +10,31 @@ public class Score implements Serializable {
 
 	public static final long serialVersionUID = 42L;
 
+	private String username;
 	private int mark;
-
-	public Score(int mark){
+	
+	public Score(String username, int mark){
+		this.username = username;
 		this.mark = mark;
 	}
 
 	int getMark(){
 		return mark;
 	}
+
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	
+	
 }
