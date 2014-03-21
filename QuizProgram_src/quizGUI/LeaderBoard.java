9183f8bc25f02931319fb5e47d1cf700a6d41417
bxc077 <bxc077@fa6e4dec-783f-4bee-b038-fdf0b38339eb>
2014-03-21 12:53:09 +0000

    LeaderBoard should now work (not updating from server still)
    git-svn-id: https://codex.cs.bham.ac.uk/svn/msc-workshop/Osaka@87 fa6e4dec-783f-4bee-b038-fdf0b38339eb

--- QuizProgram_src/quizGUI/LeaderBoard.java
+++ QuizProgram_src/quizGUI/LeaderBoard.java
@@ -3,11 +3,11 @@ package quizGUI;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Collections;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -24,57 +24,43 @@ import quizObject.Score;
  * @version 20140315
  */
 public class LeaderBoard extends MasterFrame implements Observer {
-
+   
     private static final long serialVersionUID = 1L;
-
+   
     private ArrayList<Score> allScores;
     private JTable table;
-    private String user;
-
+   
     /**
      * Constructor
      * @param model
      */
-    public LeaderBoard(ArrayList<Score> allScores, String username) {
-        this.allScores = allScores;
-        setDisplay(username);
-        setPreferredSize(new Dimension(800,250));
-    }
-
     public LeaderBoard(ArrayList<Score> allScores) {
         this.allScores = allScores;
-        //setDisplay(null);
+        setDisplay();
     }
-
+   
     /**
      * Sets the display for LeaderBoard
      */
-    public void setDisplay(String username) {
-    	
-    	this.user = username;
-    	
+    public void setDisplay() {
+       
         // The pane layout
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-
+       
         int columns = 3;
         int rows = allScores.size();
-        int position = -1; // the position of the user
 
         String[] columnNames = {"Position", "Username", "Score"};
         String[][] data = new String[rows][columns];
-
+       
         // Create 2D array for table from ArrayList<Score> allScores
         for(int i = 0; i < rows; i++) {
             data[i][0] = i + 1 + "";
             data[i][1] = allScores.get(i).getUsername();
             data[i][2] = allScores.get(i).getMark() + "";
-            if (allScores.get(i).getUsername().equalsIgnoreCase(user)) position = i;
         }
-
-//        System.out.println("AllScores Username data check: "+allScores.get(0).getUsername());
-//        System.out.println("data array: "+data[0][1]);
-
+       
         // Set up the table
         table = new JTable(data, columnNames);
         table.setFillsViewportHeight(true);
@@ -82,105 +68,47 @@ public class LeaderBoard extends MasterFrame implements Observer {
         table.getColumnModel().getColumn(1).setPreferredWidth(120);
         table.getColumnModel().getColumn(2).setPreferredWidth(70);
 
-        // Highlight the row of the current user
-        if(position >= 0) {
-            table.setRowSelectionInterval(position, position);
-        }
-
         // Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setMinimumSize(new Dimension(260,20));
+       // scrollPane.setMaximumSize(new Dimension(260,20));
         scrollPane.setPreferredSize(new Dimension(260,20));
-
+       
         // Add the scroll pane to this panel.
         c.weightx = 0; c.weighty = 1.0;
         c.fill = GridBagConstraints.VERTICAL;
         c.gridx = 0; c.gridy = 0;
         add(scrollPane, c);
     }
-
-    public static void main(String[] args) {
-
+   
+   
+   
+    public static void main(String[] args) {       
+       
         QuizClient client = new QuizClient();
-
+       
         JFrame frame = new JFrame("Quiz");
         LeaderBoard pane = new LeaderBoard(client.getAllScores());
-
+       
+       
         JFrame.setDefaultLookAndFeelDecorated(true);
         frame.setContentPane(pane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
+       
     }
-
+   
     @Override
     public void update(Observable arg0, Object arg1) {
         // TODO Auto-generated method stub
+       
+    }
 
-    }
-    
-    public void updateAllScores(ArrayList<Score> aS){
-		allScores = aS;
-    }
-    
     @Override
     public void resetDisplay() {
-    	
-    	// The pane layout
-        setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        
-        int columns = 3;
-        int rows = allScores.size();
-        int position = -1; // the position of the user
-
-        //System.out.println("NUMBER OF ROWS: "+rows);
-        
-        String[] columnNames = {"Position", "Username", "Score"};
-        String[][] data = new String[rows][columns];
-
-        // Create 2D array for table from ArrayList<Score> allScores
-        for(int i = 0; i < rows; i++) {
-            data[i][0] = i + 1 + "";
-            data[i][1] = allScores.get(i).getUsername();
-            data[i][2] = allScores.get(i).getMark() + "";
-            if (allScores.get(i).getUsername().equalsIgnoreCase(user)) position = i;
-        }
-        
-        // Set up the table
-        table = new JTable(data, columnNames);
-        table.setFillsViewportHeight(true);
-        table.getColumnModel().getColumn(0).setPreferredWidth(70);
-        table.getColumnModel().getColumn(1).setPreferredWidth(120);
-        table.getColumnModel().getColumn(2).setPreferredWidth(70);
-        
-        //System.out.println("NUMBER OF ROWS"+table.getRowCount());
-        
-        // Highlight the row of the current user
-        if(position >= 0) {
-            table.setRowSelectionInterval(position, position);
-        }
-
-        // Create the scroll pane and add the table to it.
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setMinimumSize(new Dimension(260,20));
-        scrollPane.setPreferredSize(new Dimension(260,20));
-
-        // Add the scroll pane to this panel.
-        c.weightx = 0; c.weighty = 1.0;
-        c.fill = GridBagConstraints.VERTICAL;
-        c.gridx = 0; c.gridy = 0;
-        add(table, c);
-    }
-    
-    public String toString(){
-    	
-    	String returnString = "";
-    	
-    	for(int i=0; i<allScores.size(); i++){
-    		returnString += allScores.get(i);
-    	} return returnString;
+        // TODO Auto-generated method stub
+       
     }
 
 }
--- QuizProgram_src/quizGUI/StudentResultsFrame.java
+++ QuizProgram_src/quizGUI/StudentResultsFrame.java
@@ -1,7 +1,6 @@
 package quizGUI;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -24,13 +23,10 @@ import quizObject.Score;
 public class StudentResultsFrame extends MasterFrame implements Observer {
 
     private static final long serialVersionUID = 1L;
-
+   
     // Quiz model
     QuizClient model;
-    
-    // Data
-    ArrayList<Score> allScores;
-
+   
     // GUI components
     JProgressBar progressBar = new JProgressBar(); //questions answered so far
     private JTextPane question = new JTextPane(); // Text-box displaying the question
@@ -39,36 +35,87 @@ public class StudentResultsFrame extends MasterFrame implements Observer {
     private JTextPane answerC = new JTextPane();
     private JTextPane answerD = new JTextPane();
     private LeaderBoard leaderBoard;
-    private JTextPane score = new JTextPane();
-    GridBagConstraints con = new GridBagConstraints();
-
+    private GridBagConstraints con = new GridBagConstraints();
+   
     public StudentResultsFrame(QuizClient model) {
         this.model = model;
-        allScores = model.getAllScores(); 
-        leaderBoard = new LeaderBoard(allScores);
+        setDisplay(); // TODO model.getCurrentQuestion
     }
+   
+    /**
+     * setDisplay
+     * Sets the question display based on a question object
+     *
+     * @param q - the question object to create GUI for
+     */
+    public void setDisplay() {
+   
+        // Grid layout for the answers
+        JPanel answerPane = new JPanel(new GridLayout(4,1,5,5));
+        
+        // dummy leaderBoard with no values
+        ArrayList<Score> nil = new ArrayList<Score>();
+        nil.add(new Score("", 0));
+        leaderBoard = new LeaderBoard(nil);
+       
+        question.setFont(new Font("SansSerif", Font.BOLD + Font.ITALIC, 16));
+       
+        answerPane.add(answerA);
+        answerPane.add(answerB);
+        answerPane.add(answerC);
+        answerPane.add(answerD);
 
+        // Adds the questions to the layout
+        JPanel questionPane = new JPanel(); questionPane.setLayout(new GridLayout(2,1,5,5));
+        questionPane.add(question);
+        questionPane.add(answerPane);
+       
+        // The layout for this panel
+        setLayout(new GridBagLayout());
+
+        // adding to questionPane
+        questionPane.add(question);
+        questionPane.add(answerPane);
+
+        // finally adding to main panel
+        con.fill = GridBagConstraints.BOTH;
+        con.weightx = 1.0; con.weighty = 1;
+        con.gridx = 0; con.gridy = 0; con.insets = new Insets(5,5,0,5);
+        add(questionPane, con);
+        con.fill = GridBagConstraints.VERTICAL;
+        con.weightx = 0; con.weighty = 1;
+        con.gridx = 1; con.gridy = 0; con.insets = new Insets(5,5,0,5);
+        add(leaderBoard, con);
+       
+    }
+   
     /**
      * resetDisplay - when the panel is changed to this one, some of its components will need
      * to be updated to represent the model
      */
     public void resetDisplay() {
-    	
+
         // Sets the labels to the question/answers
         Question q = model.getCurrentQuestion();
-
+       
         question.setText(q.getQuestion());
         question.setFont(new Font("SansSerif", Font.BOLD + Font.ITALIC, 16));
-
+       
         answerA.setText("A: " + q.getAnswer(0));
         answerB.setText("B: " + q.getAnswer(1));
         answerC.setText("C: " + q.getAnswer(2));
         answerD.setText("D: " + q.getAnswer(3));
-
+       
         // Highlights the correctAnswer
         Color green = new Color(10, 100, 20);
         Color red = Color.RED;
-
+        Color black = Color.BLACK;
+        
+        answerA.setForeground(black);
+        answerB.setForeground(black);
+        answerC.setForeground(black);
+        answerD.setForeground(black);
+        
         if(model.getResponseNumber() == 0) answerA.setForeground(red);
         if(model.getResponseNumber() == 1) answerB.setForeground(red);
         if(model.getResponseNumber() == 2) answerC.setForeground(red);
@@ -78,24 +125,22 @@ public class StudentResultsFrame extends MasterFrame implements Observer {
         if(q.getCorrectAnswerPos() == 1) answerB.setForeground(green);
         if(q.getCorrectAnswerPos() == 2) answerC.setForeground(green);
         if(q.getCorrectAnswerPos() == 3) answerD.setForeground(green);
+       
+        // remove the leaderboard and replace with a new one
+        remove(leaderBoard);
+        leaderBoard = new LeaderBoard(model.getAllScores());
 
-        //score.setText("Score for the last question: "+model.getScore().getMark());
-        
-        allScores = model.getAllScores();
-        leaderBoard.updateAllScores(allScores);
-        leaderBoard.resetDisplay();
-        
         con.fill = GridBagConstraints.VERTICAL;
         con.weightx = 0; con.weighty = 1;
         con.gridx = 1; con.gridy = 0; con.insets = new Insets(5,5,0,5);
-        this.add(leaderBoard, con);
-        //System.out.println("LEADERBOARD IS: "+leaderBoard);
-    }//end of reset display
-
-    @Override
-    public void update(Observable model, Object arg) {
-        // TODO Auto-generated method stub
-
+        add(leaderBoard, con);
     }
-
+   
+    @Override
+    public void update(Observable o, Object arg) {
+        // TODO Auto-generated method stub
+       
+    }
+   
+   
 }
